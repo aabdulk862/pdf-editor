03fade0620811a630cb584b711a564d2e7f78815
aabdulk862 <adama1862@outlook.com>
2026-05-23 23:05:30 -0400

    fix: add crypto polyfill for Netlify build

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,10 @@
+import { webcrypto } from 'crypto';
+
+if (!globalThis.crypto) {
+  // @ts-ignore
+  globalThis.crypto = webcrypto;
+}
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
